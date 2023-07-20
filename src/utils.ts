2cf8b8d2daa844d9b95f46f62df9899174483b77
Yan Thomas <61414485+Yan-Thomas@users.noreply.github.com>
2023-07-20 14:00:17 -0300

    Add 404 and small improvements

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
 export function formatDate(date: Date) {
   return new Intl.DateTimeFormat("en", { dateStyle: "medium" }).format(date);
 }
+
+export function ensureTrailingSlash(url: string | URL) {
+  return url.toString().replace(/\/$/, "") + "/";
+}
