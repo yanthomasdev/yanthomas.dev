b3e24f8afeb7ccc46bf333ff28056cc4a2cbf8e4
Yan <61414485+yanthomasdev@users.noreply.github.com>
2024-11-15 20:11:08 -0300

    Remove v1 code

--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-export function formatDate(date: Date) {
-  return new Intl.DateTimeFormat("en", { dateStyle: "medium" }).format(date);
-}
-
-export function ensureTrailingSlash(url: string | URL) {
-  return url.toString().replace(/\/$/, "") + "/";
-}
